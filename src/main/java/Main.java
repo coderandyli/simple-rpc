d85ecd03cb6f2f45555111f8e156624cbd2cbea2
lizhenzhen <zhenzhen@e-ports.com>
2021-06-07 11:23:36 +0800

    Project initialization

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: lizhenzhen
+ * @date: 2021-06-07 11:18
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
